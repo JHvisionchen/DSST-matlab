dbb72288f35a0851e9aa3f981aa83d8be9251aed
JHvisionchen <chenjiahong@163.com>
2019-03-04 16:13:30 +0800

    delete compile

--- dsst.m
+++ dsst.m
@@ -9,14 +9,14 @@ nScales = params.number_of_scales;
 scale_step = params.scale_step;
 scale_sigma_factor = params.scale_sigma_factor;
 scale_model_max_area = params.scale_model_max_area;
-
+% the information of images and object
 video_path = params.video_path;
 img_files = params.img_files;
 pos = floor(params.init_pos);
 target_sz = floor(params.wsize);
-
+% for visualization
 visualization = params.visualization;
-
+% the num of images
 num_frames = numel(img_files);
 
 init_target_sz = target_sz;
@@ -27,15 +27,13 @@ base_target_sz = target_sz;
 % window size, taking padding into account
 sz = floor(base_target_sz * (1 + padding));
 
-% desired translation filter output (gaussian shaped), bandwidth
-% proportional to target size
+% desired translation filter output (gaussian shaped), bandwidth proportional to target size
 output_sigma = sqrt(prod(base_target_sz)) * output_sigma_factor;
 [rs, cs] = ndgrid((1:sz(1)) - floor(sz(1)/2), (1:sz(2)) - floor(sz(2)/2));
 y = exp(-0.5 * (((rs.^2 + cs.^2) / output_sigma^2)));
 yf = single(fft2(y));
 
-% desired scale filter output (gaussian shaped), bandwidth proportional to
-% number of scales
+% desired scale filter output (gaussian shaped), bandwidth proportional to number of scales
 scale_sigma = nScales/sqrt(33) * scale_sigma_factor;
 ss = (1:nScales) - ceil(nScales/2);
 ys = exp(-0.5 * (ss.^2) / scale_sigma^2);
@@ -56,8 +54,7 @@ end;
 ss = 1:nScales;
 scaleFactors = scale_step.^(ceil(nScales/2) - ss);
 
-% compute the resize dimensions used for feature extraction in the scale
-% estimation
+% compute the resize dimensions used for feature extraction in the scale estimation
 scale_model_factor = 1;
 if prod(init_target_sz) > scale_model_max_area
     scale_model_factor = sqrt(scale_model_max_area/prod(init_target_sz));
@@ -65,13 +62,10 @@ end
 scale_model_sz = floor(init_target_sz * scale_model_factor);
 
 currentScaleFactor = 1;
-
 % to calculate precision
 positions = zeros(numel(img_files), 4);
-
 % to calculate FPS
 time = 0;
-
 % find maximum and minimum scales
 im = imread([video_path img_files{1}]);
 min_scale_factor = scale_step ^ ceil(log(max(5 ./ sz)) / log(scale_step));
@@ -79,38 +73,30 @@ max_scale_factor = scale_step ^ floor(log(min([size(im,1) size(im,2)] ./ base_ta
 
 % testVideo = VideoWriter('test.avi');
 % open(testVideo);
-
+% start tracking
 for frame = 1:num_frames,
-    %load image
+    % load image
     im = imread([video_path img_files{frame}]);
-
+    % start to timer
     tic;
-    
+    % tracking when frame > 1
     if frame > 1
-        
         % extract the test sample feature map for the translation filter
         xt = get_translation_sample(im, pos, sz, currentScaleFactor, cos_window);
-        
         % calculate the correlation response of the translation filter
         xtf = fft2(xt);
-        response = real(ifft2(sum(hf_num .* xtf, 3) ./ (hf_den + lambda)));
-        
+        response = real(ifft2(sum(hf_num .* xtf, 3) ./ (hf_den + lambda)));  
         % find the maximum translation response
-        [row, col] = find(response == max(response(:)), 1);
-        
+        [row, col] = find(response == max(response(:)), 1); 
         % update the position
-        pos = pos + round((-sz/2 + [row, col]) * currentScaleFactor);
-        
+        pos = pos + round((-sz/2 + [row, col]) * currentScaleFactor);    
         % extract the test sample feature map for the scale filter
-        xs = get_scale_sample(im, pos, base_target_sz, currentScaleFactor * scaleFactors, scale_window, scale_model_sz);
-        
+        xs = get_scale_sample(im, pos, base_target_sz, currentScaleFactor * scaleFactors, scale_window, scale_model_sz);    
         % calculate the correlation response of the scale filter
         xsf = fft(xs,[],2);
         scale_response = real(ifft(sum(sf_num .* xsf, 1) ./ (sf_den + lambda)));
-        
         % find the maximum scale response
         recovered_scale = find(scale_response == max(scale_response(:)), 1);
-        
         % update the scale
         currentScaleFactor = currentScaleFactor * scaleFactors(recovered_scale);
         if currentScaleFactor < min_scale_factor
@@ -119,23 +105,19 @@ for frame = 1:num_frames,
             currentScaleFactor = max_scale_factor;
         end
     end
-    
     % extract the training sample feature map for the translation filter
     xl = get_translation_sample(im, pos, sz, currentScaleFactor, cos_window);
-    
     % calculate the translation filter update
     xlf = fft2(xl);
     new_hf_num = bsxfun(@times, yf, conj(xlf));
     new_hf_den = sum(xlf .* conj(xlf), 3);
-    
     % extract the training sample feature map for the scale filter
     xs = get_scale_sample(im, pos, base_target_sz, currentScaleFactor * scaleFactors, scale_window, scale_model_sz);
-    
     % calculate the scale filter update
     xsf = fft(xs,[],2);
     new_sf_num = bsxfun(@times, ysf, conj(xsf));
     new_sf_den = sum(xsf .* conj(xsf), 1);
-       
+    % tracking initial
     if frame == 1
         % first frame, train with a single image
         hf_den = new_hf_den;
@@ -150,15 +132,12 @@ for frame = 1:num_frames,
         sf_den = (1 - learning_rate) * sf_den + learning_rate * new_sf_den;
         sf_num = (1 - learning_rate) * sf_num + learning_rate * new_sf_num;
     end
-    
     % calculate the new target size
     target_sz = floor(base_target_sz * currentScaleFactor);
-    
     %save position
     positions(frame,:) = [pos target_sz];
-    
+    % stop to timer
     time = time + toc;
-      
     %visualization
     if visualization == 1
         rect_position = [pos([2,1]) - target_sz([2,1])/2, target_sz([2,1])];
@@ -177,7 +156,6 @@ for frame = 1:num_frames,
                 return
             end
         end
-        
         drawnow
         frameVideo = getframe;
 %         writeVideo(testVideo,frameVideo);
